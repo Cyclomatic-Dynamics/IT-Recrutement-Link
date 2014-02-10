596e4e63a902ad39ba2b1c9996bb6f116c511f27
Chedy Missaoui <missaoui.chedy@gmail.com>
2014-02-10 15:10:42 +0100

    Created Company Entity

---
+++ IT-Recrutement-Link.Domain/Entities/ActivitySectorEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_Recrutement_Link.Domain.Entities
+{
+    public enum ActivitySectorEnum
+    {
+        Software_engeneering,
+        Telecomunication,
+        Agriculture,
+        Art
+    }
+}
---
+++ IT-Recrutement-Link.Domain/Entities/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_Recrutement_Link.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string VideoUrl { get; set; }
+        public string SlidesUrl { get; set; }
+        public string LogoPictureUrl { get; set; }
+        public ActivitySectorEnum ActivitySectorName { get; set; }
+        public String Address { get; set; }
+        public CompanySizeEnum CompanySize { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public bool? AcceptSpontanousApplication { get; set; }
+        public virtual IList<string> Keywords { get; set; }
+    }
+}
