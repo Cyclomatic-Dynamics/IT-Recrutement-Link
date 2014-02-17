30343b229c7a7dc4f6697cb6b78a9abc2d928ae4
Chedy Missaoui <missaoui.chedy@gmail.com>
2014-02-17 21:27:51 +0100

    Added DataAccess.test Project

---
+++ IT-Recrutement-Link.Domain/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_Recrutement_Link.Domain
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        
+        
+    }
+}
