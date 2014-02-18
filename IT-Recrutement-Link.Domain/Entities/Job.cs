9b63e75b99e1f28ba16934fb474d69b6df126ff8
MedAmine90 <med.amin.dr@gmail.com>
2014-02-18 13:39:00 +0100

    update

--- IT-Recrutement-Link.Domain/Entities/Job.cs
+++ IT-Recrutement-Link.Domain/Entities/Job.cs
@@ -8,5 +8,20 @@ namespace IT_Recrutement_Link.Domain.Entities
 {
     public class Job
     {
+        public int Id { get; set; }
+        public string Name;
+        private Company company;
+        public IList<Student> registeredStudents { get; private set; }
+        public Job(Company company, string Name)
+        {
+            registeredStudents = new List<Student>();
+            this.Name = Name;
+            this.company = company;
+        }
+        //gggg
+        public void Register(Student student)
+        {
+            registeredStudents.Add(student);
+        }
     }
 }
