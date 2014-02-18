a1f2171228a5069f86b9c0f9387594d5f35f7034
MedAmine90 <med.amin.dr@gmail.com>
2014-02-18 13:44:00 +0100

    update

---
+++ IT-Recrutement-Link.DataAccess/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IT_Recrutement_Link.DataAccess;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+
+namespace IT_Recrutement_Link.DataAccess.Infrastructure
+{
+   public  class UnitOfWork<T> where T :class
+    {
+       private Context context;
+       IDatabaseFactory dbfact;
+       public IDbSet<T> dbSet;
+
+       
+
+       public void UnitOfWork(Context context )
+       {
+           this.context = context;
+           dbset = context.Set<T>();
+       }
+       public void Add(T entity)
+       {
+           
+           DbSet.Add(entity);
+           context;
+           
+       }
+       public void Update(T entity)
+       {
+
+       }
+       public void Remove(T entity)
+       {
+
+       }
+       public void Find(string name)
+       {
+
+       }
+       public void Commit()
+       {
+           context.Commit();
+       }
+    }
+}
