44452a7e39fb31d63a8a644281eaad4bdb264b61
Chedy Missaoui <missaoui.chedy@gmail.com>
2014-02-18 09:59:26 +0100

    Added Test Projects for Service layer

---
+++ IT-Recrutement-Link.Domain/Entities/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_Recrutement_Link.Domain.Entities
+{
+    public class Job
+    {
+        public int Id{get;set;}
+        public string Name;
+        private Company company;
+        public IList<Student> registeredStudents { get; private set; }
+        public Job(Company company, string Name)
+        {
+            registeredStudents = new List<Student>();
+            this.Name = Name;
+            this.company = company;
+        }
+        
+        public void Register(Student student){
+            registeredStudents.Add(student);
+        }
+
+    }
+}
