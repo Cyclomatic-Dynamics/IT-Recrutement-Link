6f6cbfd33e0c24dc43040cefba3e919d6292fbff
Ahmed <ahmed.zraiga@esprit.tn>
2014-02-21 14:33:58 +0100

    new view

---
+++ IT-Recrutement-Link.Web/Controllers/CompanyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_Recrutement_Link.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public ActionResult AddCompanyForm()
+        {
+            return View();
+        }
+
+        public ActionResult EditCompanyProfil()
+        {
+            return View();
+        }
+        public ActionResult DisplayCompanyProfil()
+        {
+            return View();
+        }
+
+      
+    }
+}
