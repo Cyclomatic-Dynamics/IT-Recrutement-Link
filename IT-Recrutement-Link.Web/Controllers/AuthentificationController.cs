97c9665f99a56d009ec37b874b5f8d7183027eca
Ahmed <ahmed.zraiga@esprit.tn>
2014-02-22 20:05:26 +0100

    create authentification ("models,controllers,views")

---
+++ IT-Recrutement-Link.Web/Controllers/AuthentificationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using IT_Recrutement_Link.Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+    
+namespace IT_Recrutement_Link.Web.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl = "")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return LogOut();
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Login model, string returnUrl = "")
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(model.UserName, model.RememberMe);
+                }
+
+                ModelState.AddModelError("", "Incorrect username and/or password");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account", null);
+        }
+    }
+}
